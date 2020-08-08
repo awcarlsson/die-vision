dc7f829732758e3a42ad3b98c84c8ad662c9c119
Anders Carlsson <awcarlsson1@gmail.com>
2020-08-08 13:50:04 -0500

    number based on area

--- dievis.py
+++ dievis.py
@@ -10,9 +10,11 @@ def read_img(img_name):
     return img
 
 def resize(img):
-    scale_percent = 20 # percent of original size
-    width = int(img.shape[1] * scale_percent / 100)
-    height = int(img.shape[0] * scale_percent / 100)
+    # scale_percent = 20 # percent of original size
+    # width = int(img.shape[1] * scale_percent / 100)
+    # height = int(img.shape[0] * scale_percent / 100)
+    width = 500
+    height = int(img.shape[0]/img.shape[1] * width)
     dim = (width, height)
     img = cv2.resize(img, dim, interpolation = cv2.INTER_AREA)
     return img
@@ -27,24 +29,42 @@ def get_contours(thresh):
     contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     return contours
 
-def get_die_num(contours):
-    # get rid of areas that are too small
-    potential = contours[:]
+def get_die_num(contourss):
+    global contours
+    # get rid of areas that are too small or too large
+    potentials = []
+    contours_cop = []
     for cont in contours:
-        if cv2.contourArea(cont) > 50:
-            potential.append(cont)
+        print("found: " + str(cv2.contourArea(cont)))
+        if cv2.contourArea(cont) > 50 and cv2.contourArea(cont) < 1000:
+            contours_cop.append(cont)
+            potentials.append(cv2.contourArea(cont))
     # look for a cluster of 6-1 with similar areas
-    area_num = {}
-    for pont in potential:
-        area_num[pont] = 0
-    for pont in potential:
-        for pont2 in potential:
-            if abs(pont-pont2) < pont/3:
-                print(1)
+    contours = contours_cop
+    clusters = []
+    print(potentials)
+    for pont in potentials:
+        if len(clusters) == 0:
+            clusters.append([pont])
+        else:
+            clust_found = False
+            for i in range(len(clusters)):
+                if abs(pont-clusters[i][0]) < clusters[i][0]/3:
+                    clusters[i].append(pont)
+                    clust_found = True
+            if not clust_found:
+                clusters.append([pont])
+    lengths = []
+    for clust in clusters:
+        if len(clust) <= 6:
+            lengths.append(len(clust))
+    lengths.sort()
+    return lengths[len(lengths)-1]
 
-img_color = resize(read_img('img/top.png'))
+img_color = resize(read_img('img/top3.png'))
 thresh = get_thresh(img_color)
 contours = get_contours(thresh)
+print(get_die_num(contours))
 
 cv2.drawContours(img_color, contours, -1, (0,255,0), 3)
 cv2.imshow('im', img_color)
