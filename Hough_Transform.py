4a16e2e0b41853e5c6549b65868169d7fe81d3d2
Matt Powers <matthew.benjamin.powers@gmail.com>
2020-08-07 20:40:23 -0500

    Probabilistic Hough Line Detection

--- Hough_Transform.py
+++ Hough_Transform.py
@@ -13,33 +13,42 @@ img = cv.resize(img, dim, interpolation = cv.INTER_AREA)
 img = cv.GaussianBlur(img, (5, 5), 0)
 
 # # GRAYSCALE
-gray = cv.cvtColor(img,cv.COLOR_BGR2GRAY)
+gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 # gray = cv.GaussianBlur(gray, (3, 3), 0)
 # cv.imshow('gray', gray)
 
 # # CANNY EDGE-DETECTION
-edges = cv.Canny(gray, 0, 190, apertureSize = 3)
-edges = cv.GaussianBlur(edges, (3, 3), 0)
-cv.imshow('edges', edges)
+edges = cv.Canny(gray, 0, 150, apertureSize = 3, L2gradient=True)
+# edges = cv.GaussianBlur(edges, (3, 3), 0)
+# cv.imshow('edges', edges)
 
-# # HOUGH TRANSFORM LINE-DETECTION
-lines = cv.HoughLines(edges, 1, np.pi/180, 200)
-for line in lines:
-    rho, theta = line[0]
-    a = np.cos(theta)
-    b = np.sin(theta)
-    x0 = a*rho
-    y0 = b*rho
-    x1 = int(x0 + 1000*(-b))
-    y1 = int(y0 + 1000*(a))
-    x2 = int(x0 - 1000*(-b))
-    y2 = int(y0 - 1000*(a))
-    cv.line(img,(x1,y1),(x2,y2),(0,0,255),2)
-cv.imshow('houghlines3', img)
+# # PROBABILISTIC HOUGH TRANSFORM LINE-DETECTION
+lines = cv.HoughLinesP(edges, 1, np.pi/180, 50, minLineLength=50, maxLineGap=15)
+if lines is not None:
+    for line in lines:
+        x1,y1,x2,y2 = line[0]
+        cv.line(img,(x1,y1),(x2,y2),(0,255,0),2)
+    cv.imshow('houghlines3', img)
+
+# # # HOUGH TRANSFORM LINE-DETECTION
+# lines = cv.HoughLines(edges, 1, np.pi/180, 200)
+# if lines is not None:
+#     for line in lines:
+#         rho, theta = line[0]
+#         a = np.cos(theta)
+#         b = np.sin(theta)
+#         x0 = a*rho
+#         y0 = b*rho
+#         x1 = int(x0 + 1000*(-b))
+#         y1 = int(y0 + 1000*(a))
+#         x2 = int(x0 - 1000*(-b))
+#         y2 = int(y0 - 1000*(a))
+#         cv.line(img,(x1,y1),(x2,y2),(0,0,255),2)
+#     cv.imshow('houghlines3', img)
 
 # # LAPLACIAN
 log = cv.Laplacian(gray, cv.CV_64F, ksize=5)
-cv.imshow('lapl of gauss', log)
+# cv.imshow('lapl of gauss', log)
 
 # # EXIT
 cv.waitKey(0)
