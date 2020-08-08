888ae29e96e635db19d48663899eb9f5a742ac16
Anders Carlsson <awcarlsson1@gmail.com>
2020-08-08 14:41:06 -0500

    detects die number from video

--- dievis.py
+++ dievis.py
@@ -10,14 +10,11 @@ def read_img(img_name):
     return img
 
 def resize(img):
-    # scale_percent = 20 # percent of original size
-    # width = int(img.shape[1] * scale_percent / 100)
-    # height = int(img.shape[0] * scale_percent / 100)
     width = 500
     height = int(img.shape[0]/img.shape[1] * width)
     dim = (width, height)
-    img = cv2.resize(img, dim, interpolation = cv2.INTER_AREA)
-    return img
+    res_img = cv2.resize(img, dim, interpolation = cv2.INTER_AREA)
+    return res_img
 
 def get_thresh(img):
     imgray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -35,39 +32,62 @@ def get_die_num(contourss):
     potentials = []
     contours_cop = []
     for cont in contours:
-        print("found: " + str(cv2.contourArea(cont)))
+        #print("found: " + str(cv2.contourArea(cont)))
+        # todo: bad hardcoded nums, calculate based on image area
         if cv2.contourArea(cont) > 50 and cv2.contourArea(cont) < 1000:
             contours_cop.append(cont)
             potentials.append(cv2.contourArea(cont))
     # look for a cluster of 6-1 with similar areas
     contours = contours_cop
     clusters = []
-    print(potentials)
+    #print(potentials)
     for pont in potentials:
         if len(clusters) == 0:
             clusters.append([pont])
         else:
             clust_found = False
             for i in range(len(clusters)):
-                if abs(pont-clusters[i][0]) < clusters[i][0]/3:
+                if abs(pont-clusters[i][0]) < clusters[i][0]/4:
                     clusters[i].append(pont)
                     clust_found = True
             if not clust_found:
                 clusters.append([pont])
+    #print(clusters)
     lengths = []
     for clust in clusters:
         if len(clust) <= 6:
             lengths.append(len(clust))
     lengths.sort()
+    if len(lengths) == 0:
+        return 0
     return lengths[len(lengths)-1]
 
-img_color = resize(read_img('img/top3.png'))
-thresh = get_thresh(img_color)
-contours = get_contours(thresh)
-print(get_die_num(contours))
+# img_color = resize(read_img('img/top3.png'))
+# thresh = get_thresh(img_color)
+# contours = get_contours(thresh)
+# print(get_die_num(contours))
 
-cv2.drawContours(img_color, contours, -1, (0,255,0), 3)
-cv2.imshow('im', img_color)
-cv2.imshow('thr', thresh)
-cv2.waitKey(0)
+cap = cv2.VideoCapture(0)
+
+while True:
+    _, frame = cap.read()
+
+    img_color = resize(frame)
+    thresh = get_thresh(img_color)
+    contours = get_contours(thresh)
+    cv2.drawContours(img_color, contours, -1, (0,255,0), 3)
+    # cv2.imshow("Frame", thresh)
+    cv2.imshow("frame", img_color)
+    print(get_die_num(contours))
+
+    key = cv2.waitKey(1)
+    if key == 27:
+        break
+
+cap.release()
 cv2.destroyAllWindows()
+# cv2.drawContours(img_color, contours, -1, (0,255,0), 3)
+# cv2.imshow('im', img_color)
+# cv2.imshow('thr', thresh)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
